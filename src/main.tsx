c522881e2f008f562bee1aa88f343d2ac666d292
sebastianrodriguez <srodriguez@viapeople.com>
2025-02-01 13:17:48 -0600

    Add routing for Nelify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Navbar, HomePage, About, Projects, JumpingMice, Footer, ErrorPage, Photos } from './components';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/mgz',
+    element: <JumpingMice />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/photos',
+    element: <Photos />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+root.render(
+  <React.StrictMode>
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
+  </React.StrictMode>
+);
